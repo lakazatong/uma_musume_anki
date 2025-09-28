72346be239916f541a5bfc18af1cc1457b4d5e2f
lakazatong <Bo_wood@outlook.fr>
2025-09-28 22:59:01 +0200

    init

---
+++ anki.py
@@ -0,0 +1,217 @@
+import os
+import genanki
+import json
+
+def link_wrap(text, href, title=None):
+	if title is None:
+		title = text
+	return f'<a href="{href}" title="{title}">{text}</a>'
+
+class StableNote(genanki.Note):
+	@property
+	def guid(self):
+		return genanki.guid_for(self.fields[0], self.fields[1])
+
+css = """
+.frontbg {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: none;
+    padding: 20px;
+}
+
+.backbg {
+    display: flex;
+    align-items: flex-start;
+    background: none;
+    padding: 20px;
+}
+
+.back-left {
+    flex: 0 0 auto;
+    margin-right: 40px;
+}
+
+.back-right {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+}
+
+.name {
+    font-family: 'Noto Sans JP', 'BIZ UDGothic', sans-serif;
+    font-size: 48px;
+    font-weight: bold;
+    margin-bottom: 25px;
+    color: #fff;
+    text-align: left;
+}
+
+table.infobox {
+    border-collapse: collapse;
+    width: auto;
+}
+
+table.infobox td {
+    border: none;
+    font-size: 24px;
+    color: #eee;
+    padding: 6px 0;
+    line-height: 1.8;
+    text-align: left;
+}
+
+table.infobox td:first-child {
+    width: 140px;   /* fixed width for keys */
+    color: #ccc;
+    font-weight: bold;
+    padding-right: 100px; /* gap between key and value */
+}
+
+table.infobox td:last-child {
+    text-align: left;
+    width: auto;
+}
+
+a {
+    color: #66ccff;
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+
+img {
+    display: block;
+    height: auto;
+    border-radius: 6px;
+}
+"""
+
+class AnkiDeck:
+	def __init__(self, filename, scrap_folder):
+		self.filename = filename
+		self.scrap_folder = scrap_folder
+		self.media_files = []
+
+		self.model = genanki.Model(
+			1749272173,
+			'UmaMusume Model',
+			fields=[{'name': 'Image Tag'}, {'name': 'Name'}, {'name': 'Attributes'}],
+			templates=[{
+				'name': 'Card 1',
+				'qfmt': '''
+<div class="frontbg">
+	{{Image Tag}}
+</div>''',
+			'afmt': '''
+<div class="backbg">
+    <div class="back-left">
+        {{Image Tag}}
+    </div>
+    <div class="back-right">
+        <div class="name">{{Name}}</div>
+        {{Attributes}}
+    </div>
+</div>'''
+			}],
+			css=css
+		)
+
+		self.deck = genanki.Deck(1810934977, 'Umamusume')
+
+	def add_card(self, name, outfit):
+		safe_name = name.replace(" ", "_")
+		img_name = f"{safe_name}_({outfit}).png"
+		src_path = os.path.join(self.scrap_folder, name, img_name)
+		if not os.path.exists(src_path):
+			print(f"Warning: source image not found: {src_path}")
+			return
+
+		self.media_files.append(src_path)
+		imageTag = f'<img src="{os.path.basename(src_path)}">'
+
+		metadata_path = os.path.join(self.scrap_folder, name, "metadata.json")
+		attributes_html = ''
+		if os.path.exists(metadata_path):
+			attributes_html += '<table class="infobox"><tbody>'
+
+			with open(metadata_path, "r", encoding="utf-8") as f:
+				metadata = json.load(f)
+
+			key_order = [
+				"Kana", "Nicknames", "Alias",
+				"Birthday", "Height",
+				"Teams", "Dorm", "Roommate",
+				"Voice Actor"
+			]
+
+			for key in key_order:
+				if not (key in metadata and metadata[key].strip()):
+					continue
+				value = metadata[key]
+
+				if key == "Teams":
+					teams_links = []
+					for team in map(str.strip, value.split(",")):
+						href = f"https://umamusu.wiki/Teams_and_Clubs#Team_{team.replace(' ', '_')}"
+						teams_links.append(link_wrap(team, href))
+					value = ", ".join(teams_links)
+				elif key == "Dorm":
+					href = f"https://umamusu.wiki/Roommates/{value.replace(' ', '_')}_Dorm"
+					value = link_wrap(value, href)
+				elif key in ["Roommate", "Voice Actor"]:
+					href = f"https://umamusu.wiki/{value.replace(' ', '_')}"
+					value = link_wrap(value, href)
+
+				attributes_html += f'<tr><td><i>{key}</i></td><td>{value}</td></tr>'
+
+			attributes_html += '</tbody></table>'
+
+		name_html = link_wrap(name, f"https://umamusu.wiki/{name.replace(' ', '_')}")
+		note = StableNote(model=self.model, fields=[imageTag, name_html, attributes_html])
+		self.deck.add_note(note)
+
+	def save(self):
+		package = genanki.Package(self.deck)
+		package.media_files = self.media_files
+		package.write_to_file(self.filename)
+
+teams = {
+	"Team Spica": ["Special Week", "Silence Suzuka", "Tokai Teio", "Vodka",
+				   "Gold Ship", "Daiwa Scarlet", "Mejiro McQueen"],
+	"Team Rigil": ["Air Groove", "El Condor Pasa", "Grass Wonder", "Symboli Rudolf",
+				   "Taiki Shuttle", "T.M. Opera O", "Narita Brian", "Hishi Amazon",
+				   "Fuji Kiseki", "Maruzenski"],
+	"Team Canopus": ["Nice Nature", "Twin Turbo", "Ikuno Dictus", "Matikane Tannhauser"]
+}
+team_members = set(sum(teams.values(), []))
+
+scrap_folder = "scrap"
+all_umas = [name for name in os.listdir(scrap_folder) if os.path.isdir(os.path.join(scrap_folder, name))]
+
+deck = AnkiDeck(filename="umamusume.apkg", scrap_folder=scrap_folder)
+
+for outfit in ["Race", "Main", "Stage", "Proto"]:
+	for team, names in teams.items():
+		for name in names:
+			safe_name = name.replace(" ", "_")
+			img_name = f"{safe_name}_({outfit}).png"
+			img_path = os.path.join(name, img_name)
+			full_path = os.path.join(scrap_folder, img_path)
+			if os.path.exists(full_path):
+				deck.add_card(name, outfit)
+	for name in all_umas:
+		if name in team_members:
+			continue
+		safe_name = name.replace(" ", "_")
+		img_name = f"{safe_name}_({outfit}).png"
+		img_path = os.path.join(name, img_name)
+		full_path = os.path.join(scrap_folder, img_path)
+		if os.path.exists(full_path):
+			deck.add_card(name, outfit)
+
+deck.save()
---
+++ scrap.py
@@ -0,0 +1,238 @@
+import os, pickle, requests, time, json, re
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+from PIL import Image
+from io import BytesIO
+
+CACHE_FILE = "cache.pkl"
+OUT_FOLDER = "scrap"
+CACHE = {}
+DELAY = 1 # seconds
+TTL = 7 * 24 * 60 * 60 # 7 days
+BASE_URL = "https://umamusu.wiki"
+ATTRIBUTES = {
+	"Japanese": None,
+	"Nicknames": lambda x: ", ".join(
+		part.strip() for part in re.split(r"<br\s*/?>", x) if part.strip()
+	),
+	# Alias will be filled later
+
+	"Birthday": None,
+	"Height": None,
+
+	# Teams will be filled later
+	"Dorm": None,
+	"Roommate": lambda x: BeautifulSoup(x, "html.parser").get_text(strip=True),
+
+	"Voice Actor": lambda x: BeautifulSoup(x, "html.parser").get_text(strip=True),
+}
+
+def load_cache():
+	global CACHE
+	if os.path.exists(CACHE_FILE):
+		print("loading cache...", end=" ", flush=True)
+		with open(CACHE_FILE, "rb") as f:
+			CACHE = pickle.load(f)
+		print("done")
+
+def save_cache():
+	print("saving cache...", end=" ", flush=True)
+	with open(CACHE_FILE, "wb") as f:
+		pickle.dump(CACHE, f)
+	print("done")
+
+def load_resource(url):
+	now = time.time()
+	if url in CACHE:
+		fetched_time, soup = CACHE[url]
+		if now - fetched_time < TTL:
+			return soup
+	print(f"Fetching page: {url}")
+	time.sleep(DELAY)
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text, "html.parser")
+	CACHE[url] = (now, soup)
+	return soup
+
+def crop_transparent_pixels(im):
+	try:
+		if im.mode in ("RGBA", "LA") or (im.mode == "P" and "transparency" in im.info):
+			alpha = im.split()[-1]
+			bbox = alpha.getbbox()
+			if bbox:
+				im = im.crop(bbox)
+		return im
+	except Exception as e:
+		print(f"Failed to crop image: {e}")
+		return im
+
+def download_img(url, dest_path):
+	decoded_name = os.path.join(os.path.dirname(dest_path), unquote(os.path.basename(dest_path)))
+	if os.path.exists(decoded_name):
+		return
+	print(f"Downloading image: {decoded_name}")
+	time.sleep(DELAY)
+	r = requests.get(url)
+	im = Image.open(BytesIO(r.content))
+	im = crop_transparent_pixels(im)
+	im.save(decoded_name)
+	im.close()
+
+def find_next_sibling(start, cond):
+	sibling = start.find_next_sibling()
+	while sibling:
+		if cond(sibling):
+			return sibling
+		sibling = sibling.find_next_sibling()
+	return None
+
+def get_uma_teams():
+	teams_page = load_resource("https://umamusu.wiki/Teams_and_Clubs")
+	uma_teams = {}
+
+	for h2 in teams_page.find_all("h2"):
+		span = h2.find("span", class_="mw-headline")
+		if not span or not span.get_text(strip=True).startswith("Team "):
+			continue
+
+		team_name = span.get_text(strip=True)[len("Team "):].strip()
+
+		members_div = find_next_sibling(h2, lambda tag: tag.name == "div")
+		if not members_div:
+			continue
+
+		for box in members_div.find_all("div", class_="icon-box", recursive=False):
+			capt = box.find("div", class_="capt-box", recursive=False)
+			if not capt:
+				continue
+
+			a = capt.find("a")
+			if not (a and a.get("title")):
+				continue
+
+			name = a.get("title")
+
+			if name not in uma_teams:
+				uma_teams[name] = []
+			uma_teams[name].append(team_name)
+
+	return uma_teams
+
+def get_character_links():
+	url = BASE_URL + "/List_of_Characters"
+	soup = load_resource(url)
+
+	span = soup.find("span", {"id": "Umamusume"})
+	if not span:
+		return []
+	
+	h2 = span.find_parent("h2")
+	if not h2:
+		return []
+	
+	umamusume_div = h2.find_next_sibling("div")
+	if not umamusume_div:
+		return []
+
+	links = []
+	
+	for box in umamusume_div.find_all("div", class_="icon-box", recursive=False):
+		capt = box.find("div", class_="capt-box", recursive=False)
+		if not capt:
+			continue
+
+		a = capt.find("a")
+		if not a:
+			continue
+
+		links.append(a)
+	
+	return links
+
+def extract_images(soup, folder_name):
+	section = soup.find("section", class_="tabber__section")
+	if not section:
+		return
+	
+	for article in section.find_all("article", recursive=False):
+		first_a = article.find("a")
+		if not first_a:
+			continue
+		
+		subpage = load_resource(BASE_URL + first_a.get("href"))
+		original_file = subpage.find("a", string="Original file")
+		if not original_file:
+			continue
+		
+		img_url = BASE_URL + original_file.get("href")
+		img_name = os.path.join(folder_name, os.path.basename(img_url))
+		download_img(img_url, img_name)
+
+def extract_attributes(soup):
+	table = soup.find("table", class_="infobox")
+	if not table:
+		return {}
+
+	tbody = table.find("tbody")
+	if not tbody:
+		return {}
+	
+	attributes = {}
+
+	for tr in tbody.find_all("tr", recursive=False):
+		tds = tr.find_all("td", recursive=False)
+		if len(tds) != 2:
+			continue
+
+		key_tag = tds[0].find("i")
+		if not key_tag:
+			continue
+		
+		value_td = tds[1]
+		key = key_tag.get_text(strip=True)
+		value = value_td.decode_contents()
+
+		if key in ATTRIBUTES:
+			map_value = ATTRIBUTES[key] or (lambda x: x.strip())
+			attributes[key] = map_value(value)
+	
+	return attributes
+
+def scrap_uma(href, uma_name, teams):
+	folder_name = os.path.join(OUT_FOLDER, uma_name.replace("/", "-"))
+	os.makedirs(folder_name, exist_ok=True)
+	
+	page = load_resource(BASE_URL + href)
+
+	extract_images(page, folder_name)
+
+	attributes = extract_attributes(page)
+
+	alias_th = page.find("th", class_="infobox-subheader")
+	if alias_th:
+		i_tag = alias_th.find("i")
+		if i_tag:
+			alias = i_tag.get_text(strip=True).replace('"', '')
+			attributes["Alias"] = alias
+
+	if teams:
+		attributes["Teams"] = ", ".join(teams)
+
+	attributes_path = os.path.join(folder_name, "attributes.json")
+	with open(attributes_path, "w", encoding="utf-8") as f:
+		json.dump(attributes, f, ensure_ascii=False, indent=4)
+
+def main():
+	load_cache()
+
+	os.makedirs(OUT_FOLDER, exist_ok=True)
+	uma_teams = get_uma_teams()
+	for a in get_character_links():
+		href = a.get("href")
+		uma = a.get_text(strip=True)
+		scrap_uma(href, uma, uma_teams.get(uma, []))
+
+	save_cache()
+
+if __name__ == "__main__":
+	main()
