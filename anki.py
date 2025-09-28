f09b334271e6c96d424950b21928c9a7f4b338f7
lakazatong <Bo_wood@outlook.fr>
2025-09-28 23:36:53 +0200

    formatting, cleaned anki.py, generate teams from scrapped results

--- anki.py
+++ anki.py
@@ -1,6 +1,6 @@
-import os
+import os, json
 import genanki
-import json
+from collections import defaultdict
 
 def link_wrap(text, href, title=None):
 	if title is None:
@@ -12,89 +12,89 @@ class StableNote(genanki.Note):
 	def guid(self):
 		return genanki.guid_for(self.fields[0], self.fields[1])
 
-css = """
+CSS = """
 .frontbg {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: none;
-    padding: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: none;
+	padding: 20px;
 }
 
 .backbg {
-    display: flex;
-    align-items: flex-start;
-    background: none;
-    padding: 20px;
+	display: flex;
+	align-items: flex-start;
+	background: none;
+	padding: 20px;
 }
 
 .back-left {
-    flex: 0 0 auto;
-    margin-right: 40px;
+	flex: 0 0 auto;
+	margin-right: 40px;
 }
 
 .back-right {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
 }
 
 .name {
-    font-family: 'Noto Sans JP', 'BIZ UDGothic', sans-serif;
-    font-size: 48px;
-    font-weight: bold;
-    margin-bottom: 25px;
-    color: #fff;
-    text-align: left;
+	font-family: 'Noto Sans JP', 'BIZ UDGothic', sans-serif;
+	font-size: 48px;
+	font-weight: bold;
+	margin-bottom: 25px;
+	color: #fff;
+	text-align: left;
 }
 
 table.infobox {
-    border-collapse: collapse;
-    width: auto;
+	border-collapse: collapse;
+	width: auto;
 }
 
 table.infobox td {
-    border: none;
-    font-size: 24px;
-    color: #eee;
-    padding: 6px 0;
-    line-height: 1.8;
-    text-align: left;
+	border: none;
+	font-size: 24px;
+	color: #eee;
+	padding: 6px 0;
+	line-height: 1.8;
+	text-align: left;
 }
 
 table.infobox td:first-child {
-    width: 140px;   /* fixed width for keys */
-    color: #ccc;
-    font-weight: bold;
-    padding-right: 100px; /* gap between key and value */
+	width: 140px;   /* fixed width for keys */
+	color: #ccc;
+	font-weight: bold;
+	padding-right: 100px; /* gap between key and value */
 }
 
 table.infobox td:last-child {
-    text-align: left;
-    width: auto;
+	text-align: left;
+	width: auto;
 }
 
 a {
-    color: #66ccff;
-    text-decoration: none;
+	color: #66ccff;
+	text-decoration: none;
 }
 
 a:hover {
-    text-decoration: underline;
+	text-decoration: underline;
 }
 
 img {
-    display: block;
-    height: auto;
-    border-radius: 6px;
+	display: block;
+	height: auto;
+	border-radius: 6px;
 }
 """
 
 class AnkiDeck:
-	def __init__(self, filename, scrap_folder):
+	def __init__(self, filename, uma_folder):
 		self.filename = filename
-		self.scrap_folder = scrap_folder
+		self.uma_folder = uma_folder
 		self.media_files = []
 
 		self.model = genanki.Model(
@@ -109,16 +109,16 @@ class AnkiDeck:
 </div>''',
 			'afmt': '''
 <div class="backbg">
-    <div class="back-left">
-        {{Image Tag}}
-    </div>
-    <div class="back-right">
-        <div class="name">{{Name}}</div>
-        {{Attributes}}
-    </div>
+	<div class="back-left">
+		{{Image Tag}}
+	</div>
+	<div class="back-right">
+		<div class="name">{{Name}}</div>
+		{{Attributes}}
+	</div>
 </div>'''
 			}],
-			css=css
+			css=CSS
 		)
 
 		self.deck = genanki.Deck(1810934977, 'Umamusume')
@@ -126,7 +126,7 @@ class AnkiDeck:
 	def add_card(self, name, outfit):
 		safe_name = name.replace(" ", "_")
 		img_name = f"{safe_name}_({outfit}).png"
-		src_path = os.path.join(self.scrap_folder, name, img_name)
+		src_path = os.path.join(self.uma_folder, name, img_name)
 		if not os.path.exists(src_path):
 			print(f"Warning: source image not found: {src_path}")
 			return
@@ -134,7 +134,7 @@ class AnkiDeck:
 		self.media_files.append(src_path)
 		imageTag = f'<img src="{os.path.basename(src_path)}">'
 
-		attributes_path = os.path.join(self.scrap_folder, name, "attributes.json")
+		attributes_path = os.path.join(self.uma_folder, name, "attributes.json")
 		attributes_html = ''
 		if os.path.exists(attributes_path):
 			attributes_html += '<table class="infobox"><tbody>'
@@ -180,40 +180,63 @@ class AnkiDeck:
 		package.media_files = self.media_files
 		package.write_to_file(self.filename)
 
-teams = {
-	"Team Spica": ["Special Week", "Silence Suzuka", "Tokai Teio", "Vodka",
-				   "Gold Ship", "Daiwa Scarlet", "Mejiro McQueen"],
-	"Team Rigil": ["Air Groove", "El Condor Pasa", "Grass Wonder", "Symboli Rudolf",
-				   "Taiki Shuttle", "T.M. Opera O", "Narita Brian", "Hishi Amazon",
-				   "Fuji Kiseki", "Maruzenski"],
-	"Team Canopus": ["Nice Nature", "Twin Turbo", "Ikuno Dictus", "Matikane Tannhauser"]
-}
-team_members = set(sum(teams.values(), []))
+def parse_folder(uma_folder):
+	all_umas = []
+	teams = defaultdict(list)
 
-scrap_folder = "scrap"
-all_umas = [name for name in os.listdir(scrap_folder) if os.path.isdir(os.path.join(scrap_folder, name))]
+	for name in os.listdir(uma_folder):
+		folder_path = os.path.join(uma_folder, name)
+		if not os.path.isdir(folder_path):
+			continue
 
-deck = AnkiDeck(filename="umamusume.apkg", scrap_folder=scrap_folder)
+		all_umas.append(name)
 
-for outfit in ["Race", "Main", "Stage", "Proto"]:
-	for team, names in teams.items():
-		for name in names:
+		attributes_path = os.path.join(folder_path, "attributes.json")
+		if not os.path.exists(attributes_path):
+			continue
+
+		with open(attributes_path, "r", encoding="utf-8") as f:
+			attrs = json.load(f)
+		
+		if "Teams" not in attrs:
+			continue
+
+		for team in attrs["Teams"].split(","):
+			teams[team].append(name)
+
+	return all_umas, teams
+
+def generate_deck(uma_folder):
+	all_umas, teams = parse_folder(uma_folder)
+	team_members = set(sum(teams.values(), []))
+	deck = AnkiDeck(filename="umamusume.apkg", uma_folder=uma_folder)
+
+	for outfit in ["Race", "Main", "Stage", "Proto"]:
+		for team, names in teams.items():
+			for name in names:
+				safe_name = name.replace(" ", "_")
+				img_name = f"{safe_name}_({outfit}).png"
+				img_path = os.path.join(name, img_name)
+				full_path = os.path.join(uma_folder, img_path)
+				if os.path.exists(full_path):
+					deck.add_card(name, outfit)
+		for name in all_umas:
+			if name in team_members:
+				continue
 			safe_name = name.replace(" ", "_")
 			img_name = f"{safe_name}_({outfit}).png"
 			img_path = os.path.join(name, img_name)
-			full_path = os.path.join(scrap_folder, img_path)
+			full_path = os.path.join(uma_folder, img_path)
 			if os.path.exists(full_path):
-				deck.add_card(name, outfit)
-	for name in all_umas:
-		if name in team_members:
-			continue
-		safe_name = name.replace(" ", "_")
-		img_name = f"{safe_name}_({outfit}).png"
-		img_path = os.path.join(name, img_name)
-		full_path = os.path.join(scrap_folder, img_path)
-		if os.path.exists(full_path):
-			deck.add_card(name, outfit)
+				deck.add_card(name, outfit)	
 
-print("saving deck...", end=" ", flush=True)
-deck.save()
-print("done")
+	return deck
+
+def main():
+	deck = generate_deck("uma_musume")
+	print("saving deck...", end=" ", flush=True)
+	deck.save()
+	print("done")
+
+if __name__ == "__main__":
+	main()
